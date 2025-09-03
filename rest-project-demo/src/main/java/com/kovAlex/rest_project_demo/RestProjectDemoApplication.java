fd710f4bf4e44373926f55589936daf32f4eb92e
KovAlex17 <a.kovalev1@g.nsu.ru>
2025-09-03 18:55:09 +0300

    First elementary rest api initialization

---
+++ rest-project-demo/src/main/java/com/kovAlex/rest_project_demo/RestProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kovAlex.rest_project_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProjectDemoApplication.class, args);
+	}
+
+}
