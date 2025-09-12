b59759f49bc457406f4228ed7b888ae2294accc7
KovAlex17 <a.kovalev1@g.nsu.ru>
2025-09-12 11:06:38 +0300

    Dockerfile + docker-compose (without jar)

--- rest-project-demo/src/main/java/com/kovAlex/rest_project_demo/controllers/MainController.java
+++ rest-project-demo/src/main/java/com/kovAlex/rest_project_demo/controllers/MainController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @Tag(name = "main_methods")
